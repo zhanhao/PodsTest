491604d915b682569a0e4ec8b81449fca105eacc
zhanhao <zhanhao2037@gmail.com>
2014-07-26 14:01:35 +0800

    Initial Commit

---
+++ PodsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PodsTest
+//
+//  Created by Anim on 14-7-26.
+//  Copyright (c) 2014年 Anim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PodsTestTests/PodsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  PodsTestTests.m
+//  PodsTestTests
+//
+//  Created by Anim on 14-7-26.
+//  Copyright (c) 2014年 Anim. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PodsTestTests : XCTestCase
+
+@end
+
+@implementation PodsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
